8b25ba3b94356bbba748d2561fa9ef119c8f9dea
Макс Чирков <max.chirkov.a@gmail.com>
2023-11-16 23:04:06 +0500

    q

--- main.py
+++ main.py
@@ -3,15 +3,32 @@ from disnake.ext import commands
 
 from conf import token
 
-bot = commands.Bot(command_prefix=commands.when_mentioned)
+bot = commands.Bot(command_prefix=commands.when_mentioned, intents=disnake.Intents.all())
 
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} заробил!")
+    print(f"{bot.user} заробил!") \
+ \
+@ bot.event
+async def on_member_join(ctx):
+    embed = disnake.Embed(
+        title="Новый участник",
+        description=f"{ctx.name}#{ctx.discriminator}",
+        color=0x00FFCD
+    )
 
 
-@bot.event
-async def on_
+@bot.slash_command()
+async def server_info(inter):
+    embed = disnake.Embed(
+        title="Новый участник",
+        description=f"123",
+        color=0x00FFCD
+    )
+    await inter.response.send_message(
+        f"Название сервера: {inter.guild.name}\nКоличесво участников: {inter.guild.member_count}")
+    await inter.send(embed=embed)
+
 
 bot.run(token)
